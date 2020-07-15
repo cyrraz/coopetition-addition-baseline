2beba29fccfe13d9ef21f313a90fe1b586fae9ef
Cyrille Praz <cyrille.praz@desy.de>
2020-07-15 23:52:37 +0200

    remove comment

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ import pandas as pd
 def predict(data):
     """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)
     """
-    result = data['col1'] + data['col2'] # this is a sum
+    result = data['col1'] + data['col2']
     return result
 
 
